0d22f61e08dab5bf1a0c631a46ee42e0af059621
Mohd Arsalan Khan <arsalan787khan@gmail.com>
2025-03-06 14:31:35 +0530

    Completed UI of single & group chat

--- client/src/components/SingleChat.jsx
+++ client/src/components/SingleChat.jsx
@@ -1,14 +1,81 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { chatState } from '../context/chatProvider'
-import { Box, IconButton, Text } from '@chakra-ui/react'
-import { ArrowBackIcon } from '@chakra-ui/icons'
+import { Box, FormControl, IconButton, Input, Spinner, Text, useToast } from '@chakra-ui/react'
+import { ArrowBackIcon, ArrowForwardIcon } from '@chakra-ui/icons'
 import { getSender, getSenderFull } from '../config/ChatLogics'
 import ProfileModal from './miscellaneous/ProfileModal'
 import UpdateGroupChatModal from './miscellaneous/UpdateGroupChatModal'
+import axios from 'axios'
+import ScrollableChat from './ScrollableChat'
 
 const SingleChat = ({fetchAgain, setFetchAgain}) => {
 
+  const [messages, setMessages] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [newMessage, setNewMessages] = useState("")
+
+  const toast = useToast()
+
     const{user, selectedChat, setSelectedChat} = chatState()
+
+    const fetchMessages = async() =>{
+      if (!selectedChat) {
+        return
+      }
+
+      try {
+        setLoading(true)
+        const {data} = await axios.get(`/api/v1/message/${selectedChat._id}`,{
+          headers:{Authorization:`Bearer ${user.token}`}
+        })
+
+        console.log(messages)
+        setMessages(data)
+        setLoading(false)
+      } catch (error) {
+        toast({
+          title: "Error Occured",
+          description:"Failed to load the meesage",
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+          position: "bottom"
+        })
+      }
+    }
+    useEffect(()=>{
+      fetchMessages()
+    },[selectedChat])
+
+    const sendMessage = async(event) =>{
+      if (event.key === "Enter" && newMessage) {
+        try {
+          setNewMessages("")
+          const {data} = await axios.post("/api/v1/message",{
+            content: newMessage,
+            chatId: selectedChat._id
+          },
+          {headers: {
+            "Content-Type":"application/json",
+            Authorization: `Bearer ${user.token}`}}
+          )
+          console.log(data)
+          setMessages([...messages, data])
+        } catch (error) {
+          toast({
+            title: "Error Occured",
+            description:"Failed to send the meesage",
+            status: "error",
+            duration: 5000,
+            isClosable: true,
+            position: "bottom"
+          })
+        }
+      }
+    }
+    const typingHandler = (e) =>{
+      setNewMessages(e.target.value)
+    }
 
   return (
     <>{selectedChat?(<>
@@ -27,13 +94,38 @@ const SingleChat = ({fetchAgain, setFetchAgain}) => {
         <UpdateGroupChatModal
         fetchAgain = {fetchAgain}
         setFetchAgain = {setFetchAgain}
+        fetchMessages = {fetchMessages}
         />
         </>)}
     </Text>
     <Box display="flex" flexDirection="column" justifyContent="flex-end" padding={3} backgroundColor="#E8E8E8" width="100%"
     height="100%" borderRadius="lg" overflowY="hidden"
     >
-      {/* Messages Here */}
+      {loading ? (<Spinner
+      size="xl"
+      width={20}
+      height={20}
+      alignSelf="center"
+      margin="auto"
+      />) : (<div className='messages'>
+        <ScrollableChat messages={messages}/>
+      </div>)}
+      <FormControl display="flex" alignItems="center" isRequired marginTop={3}>
+      <Input
+        variant="Filled"
+        background="#E0E0E0"
+        placeholder="Enter your message"
+        onChange={typingHandler}
+        value={newMessage}
+        onKeyDown={sendMessage}
+      />
+      <IconButton
+        icon={<ArrowForwardIcon/>}
+        colorScheme="blue"
+        onClick={() => sendMessage({ key: "Enter" })}
+        marginLeft={2}
+      />
+</FormControl>
     </Box>
     </>):(
         <Box display="flex" alignItems="center" justifyContent="center" height="100%">
